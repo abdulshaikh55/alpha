72d5e12d2f66a48f960f8afead67be57e2509fba
abdulshaikh55 <abdulfaizshaikh55@gmail.com>
2025-08-10 11:10:40 +0530

    Add Projects and Last Visited project Section
    closes #3.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import courses from "@/assets/data/courses";
+import projects from '@/assets/data/projects';
 import LastVisitedCourseCard from "@/components/LastVisitedCourseCard";
+import LastVisitedProjectCard from "@/components/LastVisitedProjectCard";
 import SearchBar from "@/components/SearchBar";
 import { Ionicons } from "@expo/vector-icons";
 import React, { useState } from "react";
@@ -7,26 +9,25 @@ import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 const banner = require("@/assets/images/banner.png");
 
-const current_course_id: number = 3;
+const currentCourseId: number = 3;
 
-interface VProps {
-  iconName: string,
-  title: string,
+interface ValueCardProps {
+  iconName: string;
+  title: string;
 }
 
-const ValueCard: React.FC<VProps> = ({ iconName, title }) => {
-  return (
-    <View className="flex-col border-2 border-frame bg-white/10 h-36 w-36 rounded-md items-center justify-center gap-2" >
-      <Ionicons name={iconName as any} color={"#ffd33d"} size={36} />
-      <Text className="text-text_light">{title}</Text>
-    </View>
-  )
-}
+const ValueCard: React.FC<ValueCardProps> = ({ iconName, title }) => (
+  <View className="flex-col border-2 border-frame bg-white/10 h-36 w-36 rounded-md items-center justify-center gap-2">
+    <Ionicons name={iconName as any} color="#ffd33d" size={36} />
+    <Text className="text-text_light">{title}</Text>
+  </View>
+);
 
 export default function Index() {
-  let lastVisited = courses[current_course_id];
+  const lastVisited = courses[currentCourseId];
+  const project = projects[0];
   const [clicked, setClicked] = useState(false);
-  const [searchPhrase, setSearchPhrase] = useState<string>('');
+  const [searchPhrase, setSearchPhrase] = useState<string>("");
 
   return (
     <SafeAreaView className="flex-1 bg-primary_bg">
@@ -39,10 +40,14 @@ export default function Index() {
             setSearchPhrase={setSearchPhrase}
           />
         </View>
-        <Text className="text-text_light  text-2xl mt-3 font-lato_bold">Hello Rick!</Text>
+        <Text className="text-text_light text-2xl mt-3 font-lato_bold">Hello, Rick!</Text>
 
         <View className="mt-4">
-          <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ gap: 12 }}>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ gap: 12 }}
+          >
             <ValueCard iconName="medal" title="Your XP" />
             <ValueCard iconName="hammer" title="Projects" />
             <ValueCard iconName="nuclear" title="Insights" />
@@ -51,10 +56,23 @@ export default function Index() {
           </ScrollView>
         </View>
 
-        {/* User last visited course */}
+        {/* User's last visited course */}
         <View>
-          <Text className="font-semibold text-gray-300 text-lg my-3 mt-6">Last Visited</Text>
-          <LastVisitedCourseCard name={lastVisited.name} icon={lastVisited.icon} progress={0.35} />
+          <Text className="font-semibold text-gray-300 text-lg my-3 mt-6">
+            Continue where you left off
+          </Text>
+          <LastVisitedCourseCard
+            name={lastVisited.name}
+            icon={lastVisited.icon}
+            progress={0.35}
+          />
+          <View className="my-2"></View>
+          <LastVisitedProjectCard
+            name={project.name}
+            icon={project.icon}
+            progress={project.progress}
+            tags={project.technologies}
+          />
         </View>
 
         {/* Rewards Banner */}
@@ -68,4 +86,3 @@ export default function Index() {
     </SafeAreaView>
   );
 }
-
